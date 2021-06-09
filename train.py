7033f242ce3a9c276bc283e3ac68f8f147c44569
Oliver Will <oliver.will@obungi.com>
2021-06-09 17:52:54 +0200

    Initial commit

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from azureml.core import Dataset
 from sklearn.linear_model import LogisticRegression
 import argparse
 import os
@@ -14,15 +15,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,7 +41,20 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
+    return x_df, y_df
+
+
+web_paths = ['https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv']
+ds = TabularDatasetFactory.from_delimited_files(path=web_paths)
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=66)
+
+run = Run.get_context()
+
 
 def main():
     # Add arguments to script
@@ -64,9 +69,11 @@ def main():
     run.log("Max iterations:", np.int(args.max_iter))
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    joblib.dump(model, 'outputs/model.joblib')
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
 
 if __name__ == '__main__':
     main()
